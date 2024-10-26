f459a5ba2924edf7280de114a79b125ca0d7a64f
aleattene <attene.tomorrowdevs@gmail.com>
2024-10-26 18:47:44 +0200

    feat(products): add endpoint api/products and api/products/id

--- products/api/urls.py
+++ products/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ProductList, ProductDetail
+from .views import ProductListCreateApiView, ProductDetailApiView
 
 urlpatterns = [
-    path('products/', ProductList.as_view(), name='product-list'),
-    path('products/<int:pk>', ProductDetail.as_view(), name='product-detail')
+    path('products/', ProductListCreateApiView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailApiView.as_view(), name='product-detail')
 ]
