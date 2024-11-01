07c13d3616c9f84ae59b4a6b4bc31c2b75e11c00
aleattene <attene.tomorrowdevs@gmail.com>
2024-11-01 17:57:23 +0100

    feat(postgres): switch db from sqlite to postgres and add psycopg2-binary package

--- core/settings.py
+++ core/settings.py
@@ -88,14 +88,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+
+# DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
