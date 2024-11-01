14a235d3add6f16d224a997ec3c017d9793b6800
aleattene <attene.tomorrowdevs@gmail.com>
2024-11-01 13:58:04 +0100

    feat(navbar): add sportwig section-area to link templates and APIs

--- products/api/views.py
+++ products/api/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
+
+from products.api.renderers import CustomBrowsableAPIRenderer
 from products.models import Product
 from products.api.serializers import ProductSerializer
 from core.permissions import IsAdminOrReadOnly
@@ -6,7 +9,8 @@ from core.permissions import IsAdminOrReadOnly
 
 class ProductViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('price')
     serializer_class = ProductSerializer
+    # renderer_classes = [CustomBrowsableAPIRenderer, JSONRenderer]
 
 
