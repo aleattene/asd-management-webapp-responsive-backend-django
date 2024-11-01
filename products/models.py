56474d31d68ae25b7fb5dae2746d03f2df65703d
aleattene <attene.tomorrowdevs@gmail.com>
2024-11-01 21:43:19 +0100

    feat(products): add card-images handler

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     description = models.TextField(max_length=150)
     price = models.FloatField()
     available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Prodotto'
