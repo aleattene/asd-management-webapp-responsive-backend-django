3883b3d253d85860f86b54d26fbc533e464f8d27
aleattene <attene.tomorrowdevs@gmail.com>
2024-12-17 12:45:15 +0100

    feat(profiles): add model SportDoctor

---
+++ profiles/models/sport_doctors.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class SportDoctor(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Nome")
+    last_name = models.CharField(max_length=100, verbose_name="Cognome")
+    vat_number = models.CharField(max_length=11, unique=True, verbose_name="Partita IVA")
+
+    class Meta:
+        verbose_name = "Medico"
+        verbose_name_plural = "Medici"
+        # ordering = ["last_name", "first_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - P.IVA: {self.vat_number}"
+
+    def __repr__(self):
+        return (f"SportDoctor(first_name={self.first_name}, last_name={self.last_name}, "
+                f"vat_number={self.vat_number})")
+
