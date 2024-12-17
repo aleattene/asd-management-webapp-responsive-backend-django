74a2a9cd63414e1bc48fe169be20e0fa12e6e552
aleattene <attene.tomorrowdevs@gmail.com>
2024-12-17 12:44:35 +0100

    feat(profiles): add model Trainer

---
+++ profiles/models/trainers.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Nome")
+    last_name = models.CharField(max_length=100, verbose_name="Cognome")
+    fiscal_code = models.CharField(max_length=16, unique=True, verbose_name="Codice Fiscale")
+
+    class Meta:
+        verbose_name = "Allenatore"
+        verbose_name_plural = "Allenatori"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.fiscal_code})"
+
+    def __repr__(self):
+        return (f"Trainer(first_name={self.first_name}, last_name={self.last_name}, "
+                f"fiscal_code={self.fiscal_code})")
+
