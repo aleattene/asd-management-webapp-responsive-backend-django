20f0947eab2a105203dfd17d70c48fabfcceb057
aleattene <attene.tomorrowdevs@gmail.com>
2024-12-20 11:36:23 +0100

    chore(products): add temporary comments to products section

---
+++ products/api/renderers.py
@@ -0,0 +1,9 @@
+# from rest_framework.renderers import BrowsableAPIRenderer
+#
+#
+# class CustomBrowsableAPIRenderer(BrowsableAPIRenderer):
+#     def get_context(self, *args, **kwargs):
+#         context = super().get_context(*args, **kwargs)
+#         # Add custom context data here
+#         context['custom_message'] = 'Welcome to the customized API'
+#         return context
